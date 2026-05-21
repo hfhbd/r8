d7ea433e525ceea75bf8f1d6a8b9a98003cae3a6
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2026-05-21 09:37:32 +0200

    Make the versions public available (#30)

--- gradle-feature/build.gradle.kts
+++ gradle-feature/build.gradle.kts
@@ -13,15 +13,6 @@ gradlePlugin.plugins.register("io.github.hfhbd.r8.features") {
     description = "hfhbd r8 Ecosystem Plugin"
 }
 
-val version = tasks.register("writeVersion",StoreVersion::class) {
-    version.put("R8_MODULE", libs.r8.map { it.module.toString() })
-    version.put("R8_VERSION", libs.r8.map { it.version.toString() })
-}
-
-sourceSets.main {
-    kotlin.srcDir(version)
-}
-
 configurations.configureEach {
     if (isCanBeConsumed) {
         attributes {
