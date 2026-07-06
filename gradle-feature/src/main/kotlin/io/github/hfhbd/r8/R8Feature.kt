390f76080d0f645281e592aed63474c4de5888de
hfhbd <22521688+hfhbd@users.noreply.github.com>
2026-07-06 13:54:21 +0200

    DCL: Mark the apply action as safe

--- gradle-feature/src/main/kotlin/io/github/hfhbd/r8/R8Feature.kt
+++ gradle-feature/src/main/kotlin/io/github/hfhbd/r8/R8Feature.kt
@@ -2,7 +2,7 @@ package io.github.hfhbd.r8
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.dsl.ComponentMetadataHandler
+import org.gradle.api.artifacts.dsl.DependencyCollector
 import org.gradle.api.artifacts.dsl.DependencyFactory
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
@@ -30,12 +30,16 @@ interface R8BuildModel : BuildModel {
     val additionalRules: ListProperty<String>
 }
 
+abstract class DefaultR8BuildModel : R8BuildModel {
+    abstract val r8DependencyCollector: DependencyCollector
+}
+
 @BindsProjectFeature(R8Feature::class)
 abstract class R8Feature : Plugin<Project>, ProjectFeatureBinding {
     override fun apply(target: Project) {}
     override fun bind(builder: ProjectFeatureBindingBuilder) {
         builder.bindProjectFeature("r8", ApplyAction::class)
-            .withUnsafeApplyAction()
+            .withBuildModelImplementationType(DefaultR8BuildModel::class.java)
     }
 
     abstract class ApplyAction : ProjectFeatureApplyAction<R8Definition, R8BuildModel, JvmApplicationProjectType> {
@@ -62,17 +66,20 @@ abstract class R8Feature : Plugin<Project>, ProjectFeatureBinding {
             buildModel.r8Version.set(definition.r8Version.orElse(R8_VERSION))
             buildModel.additionalRules.set(definition.additionalRules)
 
-            val r8 = configurations.dependencyScope("r8") {
-                it.dependencies.addLater(buildModel.r8Version.map { r8Version ->
+            buildModel as DefaultR8BuildModel
+            buildModel.r8DependencyCollector.add(
+                buildModel.r8Version.map { r8Version ->
                     dependencyFactory.create("$R8_MODULE:$r8Version")
-                })
-            }
+                }
+            )
+
             val r8ClasspathConfig = configurations.resolvable("r8Classpath") {
-                it.extendsFrom(r8)
+                it.fromDependencyCollector(buildModel.r8DependencyCollector)
             }
 
             parentBuildModel.applications.all { application ->
                 val name = application.name.takeUnless { it == KotlinCompilation.MAIN_COMPILATION_NAME }
+
                 tasks.register("r8" + (name ?: "") , R8JarTask::class.java) {
                     it.mainClass.set(application.mainClassName)
                     it.additionalRules.addAll(buildModel.additionalRules)
